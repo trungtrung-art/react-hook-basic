834f6924f4f4d30d7cd25e15710e4be89ca0fa7b
trungtran286 <tt2861997@gmail.com>
2021-01-11 01:15:38 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
+import { useState } from 'react';
 import './App.scss';
-import ColorBox from './component/ColorBox';
+import TodoForm from './component/TodoForm';
+import TodoList from './component/TodoList';
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Welcome to my Worlds</h1>
-      
-      <ColorBox />
-    </div>
-  );
+	const [todoList, setTodoList] = useState([
+		{id: 1,title: "Mình là Thiện Trung"},
+		{id: 2,title: "Rất vui được học hỏi"},
+		{id: 3,title: "Mong mọi người giúp đỡ"}
+	])
+
+	function handleTodoClick(todo) {
+		console.log(todo);
+		const index = todoList.findIndex(x => x.id == todo.id)
+		if(index < 0) return 
+
+		// xóa todo trên App
+		const newTodoList = [...todoList];
+		newTodoList.splice(index,1);
+		// cập nhật lại TodoList
+		setTodoList(newTodoList)
+	}
+	
+	function handleTodoFormSubmit(formValues){
+		console.log('Add :',formValues.title);
+		//thêm todo mới
+
+		const newTodo = {
+			id: todoList.length + 1,
+			...formValues
+		}
+		const newTodoList = [...todoList]
+		newTodoList.push(newTodo);
+		setTodoList(newTodoList)
+	}
+
+	return (
+		<div className="app">
+			<h1>React Hook - Todo List</h1>
+			<TodoForm onSubmit={handleTodoFormSubmit}/>
+			<TodoList 
+			todos = {todoList}
+			onTodoClick = {handleTodoClick}/>
+		</div>
+	);
 }
 
 export default App;
