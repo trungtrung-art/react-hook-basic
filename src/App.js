2851ecdc25daae81a1c43fbeb10721624686baca
trungtran286 <tt2861997@gmail.com>
2021-02-11 01:21:35 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Hero from './component/Hero'
 
 function App() {
 	const [count, setCount] = useState(0);
+	const countRef = useRef(count)
+
+	useEffect(() => {
+		countRef.current = count
+		console.log("chay lai ne");
+	},[count])
 
 	return (
 		<div className="app">
 			<h1>ReactHook - Memo</h1>
+			<p>{countRef.current}</p>
 			<p>{count}</p>
 			<button onClick={() => setCount(count + 1)}>Nhấn đi</button>
 			<Hero name="Thientrung"/>
